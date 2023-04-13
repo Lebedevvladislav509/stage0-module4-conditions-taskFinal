4d908994e025dfdb2ba2f7a0ed3f72c0acb03f7a
LVLAD2009@yandex.ru <24233lebedko>
2023-04-13 13:46:27 +0300

    stage0-module4-conditions-taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        double max;
+        max = firstSide > secondSide ? firstSide : secondSide;
+        max = max > thirdSide ? max : thirdSide;
 
+        if ((firstSide + secondSide + thirdSide - max) > max) {
+            System.out.println("it's not a triangle");
+        }else{
+            System.out.println("this is a valid triangle");
+        }
     }
 }
